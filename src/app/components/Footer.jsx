9ffc66ebf7b3670b2fb6530b97894ec980cc9860
Amol <amolsg863@gmail.com>
2023-04-18 18:40:38 +0530

    footer branding added

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -40,13 +40,13 @@ const Footer = () => {
         <AppFooter>
           <FooterContent>
             <a href="https://ui-lib.com/downloads/matx-pro-react-admin/">
-              <Button variant="contained" color="secondary">
-                Get MatX Pro
-              </Button>
+{/*               <Button variant="contained" color="secondary"> */}
+{/*                 Get MatX Pro */}
+{/*               </Button> */}
             </a>
             <Span sx={{ m: 'auto' }}></Span>
-            <Paragraph sx={{ m: 0 }}>
-              Design and Developed by <a href="http://ui-lib.com">UI Lib</a>
+            <Paragraph sx={{ fontSize: '18px' }}>
+              Zolaris Energy Solutions
             </Paragraph>
           </FooterContent>
         </AppFooter>
