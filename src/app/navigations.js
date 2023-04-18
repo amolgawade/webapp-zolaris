d1b0fad9efc45ac1b3e6c123fd1dac3cb010337d
Amol <amolsg863@gmail.com>
2023-04-18 07:13:24 +0530

    changing device registration location

--- src/app/navigations.js
+++ src/app/navigations.js
@@ -8,7 +8,7 @@ export const navigations = [
       { name: 'Sign in', iconText: 'SI', path: '/session/signin' },
       { name: 'Sign up', iconText: 'SU', path: '/session/signup' },
       { name: 'Forgot Password', iconText: 'FP', path: '/session/forgot-password' },
-      { name: 'Device Registration', iconText: 'SU', path: '/session/machine-register' },
+
 //      { name: 'Error', iconText: '404', path: '/session/404' }
     ]
   },
@@ -23,7 +23,7 @@ export const navigations = [
 //      { name: 'Checkbox', path: '/material/checkbox', iconText: 'C' },
 //      { name: 'Dialog', path: '/material/dialog', iconText: 'D' },
 //      { name: 'Expansion Panel', path: '/material/expansion-panel', iconText: 'E' },
-//      { name: 'Form', path: '/material/form', iconText: 'F' },
+      { name: 'Form', path: '/material/form', iconText: 'F' },
 //      { name: 'Icons', path: '/material/icons', iconText: 'I' },
 //      { name: 'Menu', path: '/material/menu', iconText: 'M' },
 //      { name: 'Progress', path: '/material/progress', iconText: 'P' },
@@ -31,7 +31,8 @@ export const navigations = [
 //      { name: 'Switch', path: '/material/switch', iconText: 'S' },
 //      { name: 'Slider', path: '/material/slider', iconText: 'S' },
 //      { name: 'Snackbar', path: '/material/snackbar', iconText: 'S' },
-      { name: 'Table', path: '/material/table', iconText: 'T' }
+      { name: 'Table', path: '/material/table', iconText: 'T' },
+      { name: 'Device Registration', iconText: 'SU', path: '/session/machine-register' },
     ]
   },
   {
