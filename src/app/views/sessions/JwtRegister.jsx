74287935cb7b9cb02a40e8b9e112a4625df204b4
Amol <amolsg863@gmail.com>
2023-04-19 16:20:13 +0530

    validation confirm pass added

--- src/app/views/sessions/JwtRegister.jsx
+++ src/app/views/sessions/JwtRegister.jsx
@@ -50,12 +50,14 @@ const initialValues = {
   remember: true
 };
 
-// form field validation schema
 const validationSchema = Yup.object().shape({
   password: Yup.string()
-    .min(8, 'Password must be 8 character length')
-    .required('Password is required!'),
-  email: Yup.string().email('Invalid Email address').required('Email is required!')
+    .min(8, 'Password must be 8 characters')
+    .required('Password is required'),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref('password'), null], 'Passwords must match')
+    .required('Confirm password is required'),
+  email: Yup.string().email('Invalid email address').required('Email is required'),
 });
 
 const JwtRegister = () => {
